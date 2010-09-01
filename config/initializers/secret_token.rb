3dc57bcd2273f32f7dc8013118bf76208e9c2a7d
Edward O'Meara <tedomeara@edward-omearas-macbook-pro.local>
2010-08-31 20:41:36 -0400

    starting dev

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lightningfly::Application.config.secret_token = '92d37560ca0b64bcd6abeeb0975fce197db5280f990fb1e14e3525aa9730fed0dceeb86961bd10e777d8e00198c2ce014c8266fbc3459af7cc252aeb30c4f590'
